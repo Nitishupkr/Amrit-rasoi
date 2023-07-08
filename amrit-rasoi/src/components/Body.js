f753e6145d527de8a26c0c3a994b7e4132742100
Nitishupkr <nitishupkr@gmail.com>
2023-07-09 01:07:00 +0530

    commit

--- amrit-rasoi/src/components/Body.js
+++ amrit-rasoi/src/components/Body.js
@@ -1,33 +1,47 @@
 import { restrautList } from "../constant";
 import { RestrauntCard } from "../RestaurantCard";
 import { useState } from "react";
+function filterData(searchText, restaurants) {
+  const filterData = restaurants.filter((restaurant) =>
+    restaurant?.data?.name.toLowerCase().includes(searchText.toLowerCase())
+  );
+  return filterData;
+}
 const Body=()=>{
-  const [searchText,setsearchText]=useState("jbcdj");
+  const [searchText, setSearchText] = useState("");
+  const [restaurants, setRestaurants] = useState(restrautList);
     return (
 <>    
+
 <div className="search-container">
-  <input
-   type="text"
-    placeholder="search the text you want" 
-    className="search-input"
-    value={searchText}
-    onChange={(e)=>{
-setsearchText(e.target.value)
-    }}
-    ></input>
-  </div> 
-  <div className="restaurant-list">
-         {restrautList.map((restaurant)=>{
-           return <RestrauntCard {...restaurant.data}/>
-         }
- 
-         )
- 
-         }
- 
-       
-    
-     </div></>
+<i class="fa fa-search"></i>
+
+        <input
+          type="text"
+          className="search-input"
+          placeholder=""
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        ></input>
+        <button
+          className="search-btn"
+          onClick={() => {
+            // filter the data
+            const data = filterData(searchText, restaurants);
+            // update the state of restaurants list
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="restaurant-list">
+        {restaurants.map((restaurant) => {
+          return (
+            <RestrauntCard key={restaurant.data.id} {...restaurant.data} />
+          );
+        })}
+      </div></>
 
     )
  }
