b03148c0fe36aca8a8730edbbcc2ffecd0395374
Nitishupkr <nitishupkr@gmail.com>
2023-07-13 00:01:51 +0530

    Revert "commit"
    This reverts commit 99f91ebfd2fd6bc449c90ec0b671b29260b94009.

--- amrit-rasoi/src/components/Header.js
+++ amrit-rasoi/src/components/Header.js
@@ -6,27 +6,11 @@ const Title = () => (
   </a>
 );
 
-const SmallNavbar = () => {
-  return (
-    <div className="small-navbar">
-      <ul>
-        <li><a href="/">Scaling serverless Postgres Preview</a></li>
-        <li><a href="/about">|</a></li>
-        <li><a href="/contact">Learn More</a></li>
-      </ul>
-    </div>
-  );
-};
-
-
 // Header component for header section: Logo, Nav Items
 const Header = () => {
   return (
-    
     <div className="header">
-      <SmallNavbar/>
-      <h1 className="logo-header">अमृत-Rasoi</h1>
-      
+      <h2 className="logo-header">Amrit-Rasoi</h2>
       <Title />
       <div className="nav-items">
         
@@ -36,11 +20,9 @@ const Header = () => {
           <li>About</li>
           <li>Contact</li>
           <li>Services</li>
-          <li><button
-            text="Click me!"
-           onClick={()=>alert("button clicked")}
-           className="button-3"
-           >Buy Now</button></li>
+          <li>
+            <i className="fa-solid fa-cart-shopping"></i>
+          </li>
         </ul>
       </div>
     </div>
